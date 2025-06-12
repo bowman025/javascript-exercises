4b538dd6540c5930ef81cf69fc7c89d7d6c03a1a
RstK <208619198+bowman025@users.noreply.github.com>
2025-06-12 16:38:32 +0200

    Make function shorter by using arrow function

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,6 +1,4 @@
-const reverseString = function(string) {
-    return string.split('').reverse().join('');
-};
+const reverseString = string => string.split('').reverse().join('');
 
 // Do not edit below this line
 module.exports = reverseString;
