20202f1fcf14b907a1e4d4a4fde1f54effbacdfc
RstK <208619198+bowman025@users.noreply.github.com>
2025-06-24 18:33:25 +0200

    Write function for palindromes by creating and comparing two variables

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (palindrome) {
+    const palindromeFiltered = palindrome.replace(/[^a-zA-Z0-9]/g, "").toLowerCase();
+    const palindromeReversed = palindromeFiltered.split("").reverse().join("");
+    return palindromeFiltered === palindromeReversed;
 };
 
 // Do not edit below this line
