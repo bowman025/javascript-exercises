05455b206d1dc1d03ae7774cb4a03acba20d53fc
RstK <208619198+bowman025@users.noreply.github.com>
2025-06-25 14:52:41 +0200

    Write function with For Of statement and two Array methods combined

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,12 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    for (const person of people) {
+        if(!person.yearOfDeath) {
+            person.yearOfDeath = new Date().getFullYear();
+            };
+        };
+    return people
+    .sort((personA, personB) => (personB.yearOfDeath - personB.yearOfBirth) - (personA.yearOfDeath - personA.yearOfBirth))
+    .at(0);
 };
 
 // Do not edit below this line
